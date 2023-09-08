244edca2492eb7927f78c14f7441c3bea1fbfd2c
Abraham_Canas <abraham.canas2379@gmail.com>
2023-09-08 11:12:23 -0600

    Se agregó soporte a lectura del gamepad al 80%

--- GamePad.py
+++ GamePad.py
@@ -1,15 +1,12 @@
-# Control de Gamepad con Python
-
-#import evdev
 from evdev import InputDevice, categorize, ecodes
 
-#Creamos el objecto gamepad
+# Creamos el objeto gamepad
 gamepad = InputDevice('/dev/input/event8')
 
-#Muestra la info del gamepad
+# Muestra la info del gamepad
 print(gamepad)
 
-#Como las variables de python no admiten numeros usamos letras
+# Como las variables de python no admiten numeros usamos letras
 Y = 308
 A = 304
 B = 305
@@ -19,13 +16,13 @@ BACK = 314
 LOGO = 316
 LB = 310
 RB = 311
+ClicIz = 317
+ClicDer = 318
 
-
-#Muestra los codigos
+# Muestra los codigos
 for event in gamepad.read_loop():
-    #Botones
+    # Botones
     if event.type == ecodes.EV_KEY:
-        #print(event)
         if event.value == 1:
             if event.code == X:
                 print("X")
@@ -45,43 +42,59 @@ for event in gamepad.read_loop():
                 print("LB")
             elif event.code == RB:
                 print("RB")
-            elif event.value == 0:
-                print("Suelto")
-
-    #Joystick izquierdo
+            elif event.code == ClicIz:
+                print("Presion izquierda")
+            elif event.code == ClicDer:
+                print("Presion derecha")
+            elif event.code == HATOY:
+                print("Presion derecha")
+        #elif event.value == 0:
+            #print("Suelto")
+#Gatillo izquierdo
     elif event.type == ecodes.EV_ABS:
         absevent = categorize(event)
-        #print (ecodes.bytype[absevent.event.type][absevent.event.code], absevent.event.value) #"Hace lo mismo que el archivo de botones"
-        if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_X":
-             if absevent.event.value == -32768:
-                print("Izquierda")
-             elif absevent.event.value == 32767:
-                print("Derecha")
-             elif absevent.event.value == 128:
-                print("Centrado")
-        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_Y":
-             if absevent.event.value == -32768:
-                print("Arriba")
-             elif absevent.event.value == 32767:
-                print("Abajo")
-             elif absevent.event.value == -129:
-                print("Centrado")
-                
-    #Joystick derecho
-    elif event.type == ecodes.EV_ABS:
-        absevent = categorize(event)
-        #print (ecodes.bytype[absevent.event.type][absevent.event.code], absevent.event.value) #"Hace lo mismo que el archivo de botones"
-        if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_XR":
-             if absevent.event.value == -32768:
-                print("Izquierda")
-             elif absevent.event.value == 32767:
-                print("Derecha")
-             elif absevent.event.value == 128:
-                print("Centrado")
-        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_YR":
-             if absevent.event.value == -32768:
-                print("Arriba")
-             elif absevent.event.value == 32767:
-                print("Abajo")
-             elif absevent.event.value == -129:
-                print("Centrado")
+        if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_Z":
+            if absevent.event.value == 255:
+                print("Gatillo izquierdo encendido")
+            elif absevent.event.value == 0:
+                print("Gatillo izquierdo apagado")
+#Gatillo derecho
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_RZ":
+            if absevent.event.value == 255:
+                print("Gatillo derecho encendido")
+            elif absevent.event.value == 0:
+                print("Gatillo derecho apagado")
+# Joystick izquierdo
+        elif event.type == ecodes.EV_ABS:
+            absevent = categorize(event)
+            if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_X":
+                if absevent.event.value == -32768:
+                    print("Joystick Iz - Izquierda")
+                elif absevent.event.value == 32767:
+                    print("Joystick Iz - Derecha")
+                elif absevent.event.value == 128:
+                    print("Joystick Iz - Centrado")
+            elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_Y":
+                if absevent.event.value == -32768:
+                    print("Joystick Iz - Arriba")
+                elif absevent.event.value == 32767:
+                    print("Joystick Iz - Abajo")
+                elif absevent.event.value == -129:
+                    print("Joystick Iz - Centrado")     
+# Joystick izquierdo
+            elif event.type == ecodes.EV_ABS:
+                absevent = categorize(event)
+                if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_RX":
+                    if absevent.event.value == -32768:
+                        print("Joystick Der - Izquierda")
+                    elif absevent.event.value == 32767:
+                        print("Joystick Der - Derecha")
+                    elif absevent.event.value == 128:
+                        print("Joystick Der - Centrado")
+                elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_RY":
+                    if absevent.event.value == -32768:
+                        print("Joystick Der - Arriba")
+                    elif absevent.event.value == 32767:
+                        print("Joystick Der - Abajo")
+                    elif absevent.event.value == -129:
+                        print("Joystick Der - Centrado")
